67089d79ccfe7f04d072bb328783b02cbfcb2824
Nick Pelone <npelone@unca.edu>
2014-02-28 13:17:20 -0500

    Implement Sliders that update their value to DOM

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
     "use strict";
     $(document).ready(function () {
         console.log("bla");
-        $("#paddingSlider").slider();
+        $("#paddingSlider").slider({
+            value: 0,
+            orientation: "horizontal",
+            range: "min",
+            min: -100,
+            animate: true,
+            change: function(event, ui) {
+                $("#paddingText").html("Padding: " + ui.value + "px");
+            }
+        });
     });
 }());
