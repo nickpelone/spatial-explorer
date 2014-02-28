d6f3ab3a28292bac29f6ac5eff0f12151a0960d5
Nick Pelone <npelone@unca.edu>
2014-02-28 12:14:40 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/*jshint browser:true, devel:true, jquery:true */
+(function () {
+    "use strict";
+    $(document).ready(function () {
+        console.log("bla");
+        $("#paddingSlider").slider();
+    });
+}());
