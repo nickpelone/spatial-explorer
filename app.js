faf1a4a7d1565eec550e78b49f5c0359e057b3be
Nick Pelone <npelone@unca.edu>
2014-03-09 14:47:16 -0400

    work to date (sunday march 9) work to be done on css rules for arranging info boxes, placement on large screens, user interface for element manipulation

--- app.js
+++ app.js
@@ -2,15 +2,24 @@
 (function () {
     "use strict";
     $(document).ready(function () {
-        console.log("bla");
+        //populate dummy divs
+        var dummyString = "<div class='dummy'>dont mind me </div>";
+        for (var i = 0; i < 10; ++i) {
+            $("#insert").append(dummyString);
+        }
+
+        //handle slider
         $("#paddingSlider").slider({
             value: 0,
             orientation: "horizontal",
             range: "min",
             min: -100,
             animate: true,
-            change: function(event, ui) {
+            change: function (event, ui) {
                 $("#paddingText").html("Padding: " + ui.value + "px");
+                $("#target").css({
+                    "padding": "" + ui.value
+                });
             }
         });
     });
