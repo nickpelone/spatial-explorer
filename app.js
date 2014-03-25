e406581651291d22ef07629f923fe8f445c24451
Nick Pelone <npelone@unca.edu>
2014-03-25 19:16:36 -0400

    Implement margin slider

--- app.js
+++ app.js
@@ -17,7 +17,22 @@
             animate: true,
             change: function (event, ui) {
                 $("#paddingText").html("Padding: " + ui.value + "px");
+                //adjust the target div's css
                 $("#target").css({
+                    "padding": "" + ui.value
+                });
+            }
+        });
+        $("#marginSlider").slider({
+            value: 0,
+            orientation: "horizontal",
+            range: "min",
+            min: -100,
+            animate: true,
+            change: function (event, ui) {
+                $("#marginText").html("Padding: " + ui.value + "px");
+                //adjust target
+                $("target").css({
                     "padding": "" + ui.value
                 });
             }
